fb1180ec78f6ca1b68555e8173c9838315c31880
Rafael Mindreau <rafael.mindreau@gmail.com>
2021-06-26 19:10:50 +0200

    Updates

---
+++ precache-manifest.d5719f4356ed01e225566d5d1daf0ae8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ccee28af0a22d1e09fd99903bd676d",
+    "url": "/startofficer-app/index.html"
+  },
+  {
+    "revision": "3c69c1a915272e24af1f",
+    "url": "/startofficer-app/static/css/main.3da1d74c.chunk.css"
+  },
+  {
+    "revision": "259b51d66f73ffa530b2",
+    "url": "/startofficer-app/static/js/2.add7e14f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/startofficer-app/static/js/2.add7e14f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c69c1a915272e24af1f",
+    "url": "/startofficer-app/static/js/main.dacc96e9.chunk.js"
+  },
+  {
+    "revision": "247ed3348ae01d650eae",
+    "url": "/startofficer-app/static/js/runtime-main.593cbaf1.js"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/startofficer-app/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/startofficer-app/static/media/Poppins-Regular.8b6af8e5.ttf"
+  }
+]);
